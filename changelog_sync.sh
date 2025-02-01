9c548625395d7b7e3fece144dab1c11aae3ff304
Matthieu Pignolet <m@mpgn.dev>
2025-02-01 12:25:28 +0000

    finish github actions

--- changelog_sync.sh
+++ changelog_sync.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+# Release information variables
+VERSION=$(cd frr; git describe --tags --abbrev=0 | sed -e "s/^frr-//")
+DATE=$(date -R)
+
+# Removing the current changelog file
 0> debian/changelog
+
+# Adding the new patch-ed release
 cat <<EOF >> debian/changelog
-frr (8.5.2-0+orion) unstable; urgency=medium
+frr ($VERSION+orion) unstable; urgency=medium
 
   * Enable necessary FRR daemons for Orion (bgp and pim)
 
- -- Matthieu Pignolet <m@mpgn.dev>  Fri, 24 Jan 2025 13:47:59 +0400
+ -- Matthieu Pignolet <m@mpgn.dev>  $DATE
 
 EOF
 
+# Appending the remaining release information
 cat frr/debian/changelog >> debian/changelog
